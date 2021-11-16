4039e600c1d0e318d1c2ff57b0034e3457153069
Nicolas Posa <nicoposa57@gmail.com>
2021-11-16 15:45:39 -0300

    functions in solidity

--- identity.sol
+++ identity.sol
@@ -9,9 +9,9 @@ contract Identity {
   address public wallet;
     
   constructor(uint _idNumber, bool _isWorking, string memory _name){
-      idNumber = _idNumber;
-      isWorking = _isWorking;
-      name = _name;
-      wallet = msg.sender;
+    idNumber = _idNumber;
+    isWorking = _isWorking;
+    name = _name;
+    wallet = msg.sender;
   }
 }
